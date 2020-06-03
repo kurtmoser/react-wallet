d66012d8719b7656054f8b712cf1869b53730b0a
Kurt Moser <kurt@suvaline.com>
2020-06-03 12:46:42 +0300

    Add app navbar

--- src/frontend/src/components/ExpenseEdit.js
+++ src/frontend/src/components/ExpenseEdit.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import ExpenseForm from './ExpenseForm';
+import { AppBar, Toolbar } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { withRouter } from 'react-router-dom';
 
 export class ExpenseEdit extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleBack = this.handleBack.bind(this);
+  }
+
+  handleBack() {
+    this.props.history.replace('/');
+  }
+
   render() {
     return (
       <div>
+        <AppBar position="static">
+          <Toolbar>
+            <ArrowBackIcon onClick={this.handleBack} style={{ cursor: 'pointer' }} />
+          </Toolbar>
+        </AppBar>
+
         <ExpenseForm expenseId={this.props.match.params.id} />
       </div>
     )
   }
 }
 
-export default ExpenseEdit
+export default withRouter(ExpenseEdit);
--- src/frontend/src/components/Home.js
+++ src/frontend/src/components/Home.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import { AppBar, Grid, Toolbar } from '@material-ui/core';
 import ExpenseForm from './ExpenseForm';
 import ExpensesTable from './ExpensesTable';
+import {
+  format as dateFormat,
+  parse as dateParse,
+  startOfDay as dateStartOfDay,
+  startOfWeek as dateStartOfWeek,
+} from 'date-fns';
 
 export class Home extends Component {
   constructor(props) {
@@ -9,20 +16,55 @@ export class Home extends Component {
 
     this.state = {
       expenses: null,
+      dailyExpenseSum: 0,
+      weeklyExpenseSum: 0,
     }
   }
 
   async componentDidMount() {
     const expenses = (await axios.get('http://localhost:8000/expenses')).data;
 
+    const startOfWeek = dateStartOfWeek(new Date(), { weekStartsOn: 1 });
+    const startOfDay = dateStartOfDay(new Date(), { weekStartsOn: 1 });
+
+    let dailyExpenseSum = 0;
+    let weeklyExpenseSum = 0;
+
+    expenses.map((expense) => {
+      const spendDate = dateParse(expense.sdate, 'yyyy-MM-dd', new Date());
+
+      if (spendDate >= startOfWeek) {
+        weeklyExpenseSum += expense.amount;
+      }
+
+      if (spendDate >= startOfDay) {
+        dailyExpenseSum += expense.amount;
+      }
+    });
+
     this.setState({
-      expenses
+      expenses,
+      dailyExpenseSum,
+      weeklyExpenseSum,
     });
   }
 
   render() {
     return (
       <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Grid container>
+              <Grid item xs={6}>
+                Daily: &euro; {this.state.dailyExpenseSum.toFixed(2)}
+              </Grid>
+              <Grid item xs={6} align="right">
+                Weekly: &euro; {this.state.weeklyExpenseSum.toFixed(2)}
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+
         <ExpenseForm />
         <ExpensesTable expenses={this.state.expenses} />
       </div>
@@ -30,4 +72,4 @@ export class Home extends Component {
   }
 }
 
-export default Home
+export default Home;
