35e233bfb2e0516acba3b9e3142d2fc522b49ef9
Kurt Moser <kurt@suvaline.com>
2020-06-05 11:40:39 +0300

    Tweak expenses tablerow layout

--- src/frontend/src/components/ExpensesTable.js
+++ src/frontend/src/components/ExpensesTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 export class ExpensesTable extends Component {
   render() {
@@ -13,16 +14,21 @@ export class ExpensesTable extends Component {
               this.props.expenses && this.props.expenses.map((expense) => (
                 <TableRow key={expense.id}>
                   <TableCell>{expense.location} - {expense.goods}<br/>{expense.sdate}</TableCell>
-                  <TableCell align="right">
-                    &euro; {expense.amount}
-                    <Link to={`/expenses/${expense.id}`}>
-                      <Button
-                        variant="outlined"
-                        style={{marginLeft: 8}}
-                      >
-                        Edit
-                      </Button>
-                    </Link>
+                  <TableCell>
+                    <div style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'flex-end',
+                    }}>
+                      <div style={{border: 'solid 0px #f00'}}>
+                        &euro; {expense.amount.toFixed(2)}
+                      </div>
+                      <div style={{display: 'flex', border: 'solid 0px #f00', marginLeft: 16}}>
+                        <Link to={`/expenses/${expense.id}`} style={{display: 'flex'}}>
+                          <MoreVertIcon style={{border: 'solid 0px #0f0', color: 'rgba(0, 0, 0, 0.5)'}}/>
+                        </Link>
+                      </div>
+                    </div>
                   </TableCell>
                 </TableRow>
               ))
